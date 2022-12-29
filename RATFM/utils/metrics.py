998ee980411a5331f8a658c260cb93cc5022ce36
liulingbo918 <liulingbo918@gmail.com>
2022-12-29 16:08:42 +0800

    Update metrics.py

--- RATFM/utils/metrics.py
+++ RATFM/utils/metrics.py
@@ -13,3 +13,9 @@ def get_MAPE(pred, real):
         er_sum = np.sum(np.abs(real[i] - pred[i]))
         mapes.append(er_sum / gt_sum)
     return np.mean(mapes)
+
+def get_p2p_MAPE(pred, real):
+    ori_real = real.copy()
+    epsilon = 1 # if use small number like 1e-5 resulting in very large value
+    real[real == 0] = epsilon 
+    return np.mean(np.abs(ori_real - pred) / real)
