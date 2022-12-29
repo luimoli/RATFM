0a24c739c12cf0ceab029a9f1d14355e0e243659
liulingbo918 <liulingbo918@gmail.com>
2022-12-29 16:27:04 +0800

    Update misc.py

--- RATFM/utils/misc.py
+++ RATFM/utils/misc.py
@@ -66,7 +66,7 @@ def get_topk_vector(arr, mask):
     return res
 
 
-def nested_tensor_from_tensor_list(tensor_list: List[Tensor]):
+def get_nested_tensor(tensor_list: List[Tensor]):
     # # TODO make this more general
     # if tensor_list[0].ndim == 3:
     #     if torchvision._is_tracing():
@@ -78,18 +78,12 @@ def nested_tensor_from_tensor_list(tensor_list: List[Tensor]):
     # max_size = _max_by_axis([list(img.shape) for img in tensor_list])
     # # min_size = tuple(min(s) for s in zip(*[img.shape for img in tensor_list]))
     # batch_shape = [len(tensor_list)] + max_size
-    batch_shape = tensor_list.shape
+    
     b, c, h, w = tensor_list.shape
-    dtype = tensor_list[0].dtype
     device = tensor_list[0].device
-    tensor = torch.zeros(batch_shape, dtype=dtype, device=device)
-    mask = torch.ones((b, h, w), dtype=torch.bool, device=device)
-    for img, pad_img, m in zip(tensor_list, tensor, mask):
-        pad_img[: img.shape[0], : img.shape[1], : img.shape[2]].copy_(img)
-        m[: img.shape[1], :img.shape[2]] = False
-    # else:
-    #     raise ValueError('not supported')
-    return NestedTensor(tensor, mask)
+    mask = torch.zeros((b, h, w), dtype=torch.bool, device=device)
+    
+    return NestedTensor(tensor_list, mask)
 
 
 class NestedTensor(object):
